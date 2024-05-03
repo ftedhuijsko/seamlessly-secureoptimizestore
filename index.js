554b2b257f89a614fd3609031ab6c4ddc4f0c2ed
ftedhuijsko <hsjcelmuthmatos@hotmail.com>
2024-05-03 17:28:08 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = Array.from(Array(m), () => Array(n).fill(0));
-  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (obstacleGrid[i][j] === 1) continue;
-      if (i > 0) dp[i][j] += dp[i - 1][j];
-      if (j > 0) dp[i][j] += dp[i][j - 1];
+function findMaxLength(nums) {
+  const map = new Map();
+  map.set(0, -1);
+  let count = 0;
+  let maxLength = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i] === 1 ? 1 : -1;
+    if (map.has(count)) {
+      maxLength = Math.max(maxLength, i - map.get(count));
+    } else {
+      map.set(count, i);
     }
   }
-  return dp[m - 1][n - 1];
+  return maxLength;
 }
